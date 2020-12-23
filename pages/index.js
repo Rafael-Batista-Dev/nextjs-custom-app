59afe62a25166d4b19fc8998d66d40f1e5ece175
Rafael-Batist-Dev <dev.rafaelbatista@gmail.com>
2020-12-23 12:01:35 -0300

    axios nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import axios from "axios";
 
-export default function Home() {
+function Home({ users }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,55 +12,38 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Lista de <i>usuários!</i>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {users.map((user) => (
+            <div className={styles.card} key={user.id}>
+              <h3>{user.name}</h3>
+            </div>
+          ))}
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
+
+export async function getServerSideProps(context) {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  const data = await response.data;
+
+  return {
+    props: { users: data }, // will be passed to the page component as props
+  };
+}
+
+export default Home;
